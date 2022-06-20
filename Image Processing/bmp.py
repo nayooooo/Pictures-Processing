9116959e1f2aab1a0598420809fd6169241a0701
nayooooo <97676318+nayooooo@users.noreply.github.com>
2022-06-21 00:39:08 +0800

    Add files via upload

--- Image Processing/bmp.py
+++ Image Processing/bmp.py
@@ -1,66 +1,342 @@
 import os
+import numpy as np
 from PIL import Image
+import matplotlib.image as mpimg
 
 
-class PictureToBmp(object):
+class Picture2Bmp(object):
     """将PNG转成BMP"""
 
-    def __init__(self, pictureRoute="", pictureFormat="", folderRoute='', saveRoute=''):
+    def __init__(self, pictureRoute="", folderRoute='', saveRoute=''):
         """
         初始化对象
         :param pictureRoute: 原始图片位置
-        :param pictureFormat: 图片格式
+        :param folderRoute: 文件夹路径
+        :param saveRoute: 保存路径
         """
 
         self.pictureRoute = pictureRoute
-        self.pictureFormat = pictureFormat
         self.folderRoute = folderRoute
         self.saveRoute = saveRoute
 
     def __del__(self):
         pass
 
-    def jpegToBmp(self):
-        """将JPEG转成BMP"""
+    def picture2Bmp(self, show=False, save=False):
+        """
+        将JPG转成BMP
+        :param show: 是否显示
+        :param save: 是否保存
+        :return: 保存的BMP的路径
+        """
 
         if self.pictureRoute != '':
-            if os.path.splitext(self.pictureRoute)[1] == '.jpeg':
-                originalFileRoute = self.pictureRoute
-                originalFileRoute_list = list(originalFileRoute)
+            # 得到图片原始名字
+            originalFillName = os.path.basename(self.pictureRoute)
 
-                # 得到图片名字
-                dirIndex = []
-                for i in range(len(originalFileRoute)):
-                    if originalFileRoute_list[i] == '/':
-                        dirIndex.append(i)
-                dirPointIndex = []
-                for i in range(len(originalFileRoute)):
-                    if originalFileRoute_list[i] == '.':
-                        dirPointIndex.append(i)
-                name = originalFileRoute_list[dirIndex[-1]+1:dirPointIndex[-1]]
-                name = ''.join(name)
+            # 得到图片名字
+            originalFillName_list = list(originalFillName)
+            dirPointIndex = []
+            for i in range(len(originalFillName_list)):
+                if originalFillName_list[i] == '.':
+                    dirPointIndex.append(i)
+            originalFillName_list = originalFillName_list[:dirPointIndex[-1]]
+            name = ''.join(originalFillName_list)
 
-                img = Image.open(self.pictureRoute)
-                img.save(self.saveRoute+'/'+name+'.bmp')
+            img = Image.open(self.pictureRoute)
+            if show:
+                img.show()
+            if save:
+                bmpRoute = os.path.join(self.saveRoute, (name + '.bmp'))
+                img.save(bmpRoute)
+                return bmpRoute
 
-    def jpegToBmp_Folder(self):
-        """将文件夹中的JPEG转成BMP"""
+        else:
+            print('Picture2Bmp.jpg2Bmp ERROR 0')
+
+    def pictures2Bmp_Folder(self, show=False, save=False):
+        """
+        将文件夹中的JPG转成BMP
+        :param show: 是否显示
+        :param save: 是否保存
+        :return: 保存的BMP的路径（列表）
+        """
 
         if self.folderRoute != '':
+            saveRoute = []
+            saveRouteIndex = 0
             for name in os.listdir(self.folderRoute):
-                if os.path.splitext(name)[1] == '.jpeg':
+                fileSuffix = os.path.splitext(name)[1]
+
+                # 当文件有后缀名时
+                if fileSuffix != '':
                     # 得到图片名字
                     name_list = list(name)
                     dirPointIndex = []
                     for i in range(len(name)):
                         if name_list[i] == '.':
                             dirPointIndex.append(i)
-                    name = name_list[0:dirPointIndex[-1]]
-                    name = ''.join(name)
+                    name_list = name_list[:dirPointIndex[-1]]
+                    name = ''.join(name_list)
 
-                    img = Image.open(self.folderRoute+'/'+name+'.jpeg')
-                    img.save(self.saveRoute+'/'+name+'.bmp')
+                    img = Image.open(os.path.join(self.folderRoute, (name + fileSuffix)))
+                    if show:
+                        img.show()
+                    if save:
+                        saveRoute.append(os.path.join(self.saveRoute, (name + '.bmp')))
+                        img.save(saveRoute[saveRouteIndex])
+                        saveRouteIndex += 1
+                else:
+                    print('Picture2Bmp.pictures2Bmp_Folder ERROR 1')
+                    continue
+            return saveRoute
+        else:
+            print('Picture2Bmp.pictures2Bmp_Folder ERROR 0')
+            return
+
+
+class CutBmp(object):
+    """裁剪BMP"""
+
+    def __init__(self, bmpRoute='', folderRoute='', saveRoute=''):
+        """
+        初始化对象
+        :param bmpRoute: BMP路径
+        :param folderRoute: 文件夹路径
+        :param saveRoute: 保存路径
+        """
+
+        self.bmpRoute = bmpRoute
+        self.folderRoute = folderRoute
+        self.saveRoute = saveRoute
+
+    def __del__(self):
+        pass
+
+    def section(self, row, col, rowOffset, colOffset, show=False, save=False):
+        """
+        使用切片法将BMP图像裁剪为合适的大小
+        :param row: 切片后行数
+        :param col: 切片后列数
+        :param rowOffset: 行偏移，取行正向为正
+        :param colOffset: 列偏移，取列正向为正
+        :param show: 是否显示
+        :param save: 是否保存
+        :return: 保存的BMP的路径
+        """
+
+        bmp_mat = mpimg.imread(self.bmpRoute)
+        bmpRow = bmp_mat.shape[0]
+        bmpCol = bmp_mat.shape[1]
+
+        # 判断是否适合裁剪，若适合，则进行裁剪
+        if (bmpRow >= row + abs(rowOffset)) and (bmpCol >= col + abs(colOffset)):
+            rowCutFor = (bmpRow + 1 - row) // 2 + rowOffset
+            colCutFor = (bmpCol + 1 - col) // 2 + colOffset
+            newBmp_mat = bmp_mat[rowCutFor:rowCutFor + row, colCutFor:colCutFor + col]
+        else:
+            print('CutBmp.section ERROR 0')
+            return
+
+        # 显示与保存
+        newBmp_mat = Image.fromarray(newBmp_mat)
+        if show:
+            newBmp_mat.show()
+        if save:
+            saveRoute = os.path.join(self.saveRoute, os.path.basename(self.bmpRoute))
+            newBmp_mat.save(saveRoute)
+            return saveRoute
+
+
+class Bmp2BWP(object):
+    """将BMP转成黑白图"""
+
+    def __init__(self, bmpRoute="", folderRoute='', saveRoute=''):
+        """
+        初始化对象
+        :param bmpRoute: 原始图片位置
+        :param folderRoute: 文件夹路径
+        :param saveRoute: 保存路径
+        """
+
+        self.bmpRoute = bmpRoute
+        self.folderRoute = folderRoute
+        self.saveRoute = saveRoute
+
+    def __del__(self):
+        pass
+
+    def bmp2BWP(self, flip=False, show=False, save=False):
+        """
+        将BMP转成黑白图
+        :param flip: 是否黑白反转
+        :param show: 是否显示
+        :param save: 是否保存
+        :return: 保存的BWP的路径
+        """
+
+        if self.bmpRoute != '':
+            bmp_mat = mpimg.imread(self.bmpRoute)
+            bmpRow = bmp_mat.shape[0]
+            bmpCol = bmp_mat.shape[1]
+
+            # 将图片处理成极黑极白
+            for i in range(bmpRow):
+                for j in range(bmpCol):
+                    temp = 0
+                    for k in range(3):
+                        temp += bmp_mat[i, j][k]
+                    temp /= 3
+                    if temp > 128:
+                        if flip:
+                            bmp_mat[i, j] = [0, 0, 0]
+                        else:
+                            bmp_mat[i, j] = [255, 255, 255]
+                    else:
+                        if flip:
+                            bmp_mat[i, j] = [255, 255, 255]
+                        else:
+                            bmp_mat[i, j] = [0, 0, 0]
+
+            bmp_mat = Image.fromarray(bmp_mat)
+            bmp_mat = bmp_mat.convert('L')  # 转成黑白图
+            if show:
+                bmp_mat.show()
+            if save:
+                saveRoute = os.path.join(self.saveRoute, os.path.basename(self.bmpRoute))
+                bmp_mat.save(saveRoute)
+                return saveRoute
+        else:
+            print('Bmp2BWP.bmp2BWP ERROR 0')
+
+    def bmp2BWP_Folder(self, flip=False, show=False, save=False):
+        """
+        将BMP转成黑白图
+        :param flip: 是否黑白反转
+        :param show: 是否显示
+        :param save: 是否保存
+        :return: 保存的BWP的路径（列表）
+        """
+
+        if self.folderRoute != '':
+            saveRoute = []
+            saveRouteIndex = 0
+            for name in os.listdir(self.folderRoute):
+                fileSuffix = os.path.splitext(name)[1]
+
+                if fileSuffix != '':
+                    bmp_mat = mpimg.imread(os.path.join(self.folderRoute, name))
+                    bmpRow = bmp_mat.shape[0]
+                    bmpCol = bmp_mat.shape[1]
+
+                    # 将图片处理成极黑极白
+                    for i in range(bmpRow):
+                        for j in range(bmpCol):
+                            temp = 0
+                            for k in range(3):
+                                temp += bmp_mat[i, j][k]
+                            temp /= 3
+                            if temp > 128:
+                                if flip:
+                                    bmp_mat[i, j] = [0, 0, 0]
+                                else:
+                                    bmp_mat[i, j] = [255, 255, 255]
+                            else:
+                                if flip:
+                                    bmp_mat[i, j] = [255, 255, 255]
+                                else:
+                                    bmp_mat[i, j] = [0, 0, 0]
+
+                    bmp_mat = Image.fromarray(bmp_mat)
+                    bmp_mat = bmp_mat.convert('L')  # 转成黑白图
+                    if show:
+                        bmp_mat.show()
+                    if save:
+                        saveRoute.append(os.path.join(self.saveRoute, name))
+                        bmp_mat.save(saveRoute[saveRouteIndex])
+                        saveRouteIndex += 1
+
+                else:
+                    print('Bmp2BWP.bmp2BWP_Folder ERROR 1')
+                    continue
+            return saveRoute
+        else:
+            print('Bmp2BWP.bmp2BWP_Folder ERROR 0')
+            return
+
+
+class PrintText(object):
+    """将黑白图输出成文本"""
+
+    def __init__(self, bmpRoute='', saveRoute=''):
+        """
+        初始化对象
+        :param bmpRoute: BMP路径
+        :param saveRoute: 保存路径
+        """
+
+        self.bmpRoute = bmpRoute
+        self.saveRoute = saveRoute
+
+    def __del__(self):
+        pass
+
+    def printText(self, show=False, save=False):
+        """
+        将黑白图输出成十六进制文本
+        :param show: 是否显示
+        :param save: 是否保存
+        :return: 保存的TXT的路径
+        """
+
+        if self.bmpRoute != '':
+            fileSuffix = os.path.splitext(self.bmpRoute)[1]
+            if fileSuffix != '':
+                text = np.mat(np.random.randint(0, 1, (128, 8)))  # 这个矩阵为了适合OLED，应竖着看
+                bmp_mat = mpimg.imread(self.bmpRoute)
+                bmpRow = bmp_mat.shape[0]
+                bmpCol = bmp_mat.shape[1]
+
+                # 计算得到对应的OLED图片数组
+                for i in range(bmpCol):  # 列
+                    for j in range(bmpRow//8):  # 页
+                        for k in range(8):  # 字节
+                            text[i, j] <<= 1
+                            if bmp_mat[j*8+k, i] > 0:
+                                text[i, j] += 1
+
+                # 得到文件名字
+                name = os.path.basename(self.bmpRoute)
+                name_list = list(name)
+                dirPointIndex = []
+                for i in range(len(name_list)):
+                    if name_list[i] == '.':
+                        dirPointIndex.append(i)
+                bmpName_list = name_list[:dirPointIndex[-1]]
+                name = ''.join(bmpName_list)
+
+                if show:
+                    print(text)
+                if save:
+                    text = np.array(text)
+                    saveRoute = os.path.join(self.saveRoute, (name + '.txt'))
+                    np.savetxt(fname=saveRoute, X=text, fmt='\t0X%X',
+                               delimiter=',', newline=',\n',
+                               header='{', footer='\r}', comments='')
+                    return saveRoute
+
+            else:
+                print('PrintText.printText ERROR 1')
+                return
+
+        else:
+            print('PrintText.printText ERROR 0')
+            return
 
 
 if __name__ == '__main__':
-    pass
+    test = np.mat(np.random.randint(0, 1, (3, 4)))
+    row = test.shape[0]
+    col = test.shape[1]
+
+    print(test, row, col)
--- Image Processing/main.py
+++ Image Processing/main.py
@@ -1,13 +1,149 @@
 import bmp
+import os
 
 __name__ = '__main__'
 
 
+def demo_0(pictureRoute='', folderRoute='', saveRoute='', show=False, save=False):
+    """
+    将图片转成BMP
+    :param pictureRoute: 图片路径
+    :param folderRoute: 文件夹路径
+    :param saveRoute: 保存路径
+    :param show: 是否显示
+    :param save: 是否保存
+    """
+
+    if pictureRoute != '':
+        BMP = bmp.Picture2Bmp(pictureRoute=pictureRoute, saveRoute=saveRoute)
+        BMP.picture2Bmp(show=show, save=save)
+    if folderRoute != '':
+        BMP = bmp.Picture2Bmp(folderRoute=folderRoute, saveRoute=saveRoute)
+        BMP.picture2Bmp(show=show, save=save)
+    print('demo_1 done')
+
+
+def demo_1(bmpRoute='', folderRoute='', saveRoute='', flip=False, show=False, save=False):
+    """
+    将BMP转成黑白图
+    :param bmpRoute: BMP路径
+    :param folderRoute: 文件夹路径
+    :param saveRoute: 保存路径
+    :param flip: 是否黑白反转
+    :param show: 是否显示
+    :param save: 是否保存
+    """
+
+    if bmpRoute != '':
+        BMP = bmp.Bmp2BWP(bmpRoute=bmpRoute, saveRoute=saveRoute)
+        BMP.bmp2BWP(flip=flip, show=show, save=save)
+    if folderRoute != '':
+        BMP = bmp.Bmp2BWP(folderRoute=folderRoute, saveRoute=saveRoute)
+        BMP.bmp2BWP_Folder(flip=flip, show=show, save=save)
+
+
+def demo_2(bmpRoute='', saveRoute='', row=64, col=128, rowOffset=0, colOffset=0, show=False, save=False):
+    """
+    裁剪BMP
+    :param bmpRoute: BMP路径
+    :param saveRoute: 保存路径
+    :param row: 裁剪后的行数
+    :param col: 裁剪后的列数
+    :param rowOffset: 行偏移
+    :param colOffset: 列偏移
+    :param show: 是否显示
+    :param save: 是否保存
+    """
+
+    if bmpRoute != '':
+        BMP = bmp.CutBmp(bmpRoute=bmpRoute, saveRoute=saveRoute)
+        BMP.section(row=row, col=col, rowOffset=rowOffset, colOffset=colOffset, show=show, save=save)
+
+
+def demo_3(bmpRoute='', saveRoute='', show=False, save=False):
+    """
+    将黑白图保存成十六进制TXT
+    :param bmpRoute: BMP路径
+    :param saveRoute: 保存路径
+    :param show: 是否显示
+    :param save: 是否保存
+    """
+
+    if bmpRoute != '':
+        BMP = bmp.PrintText(bmpRoute=bmpRoute, saveRoute=saveRoute)
+        BMP.printText(show=show, save=save)
+
+
+def jpeg2Txt(pictureRoute='', folderRoute='', saveRoute='',
+             flip=False, row=64, col=128, rowOffset=0, colOffset=0,
+             showEveryStep=False, saveEveryStep=False,
+             showBmp=False, showCutBmp=False, showBWP=False, showTxt=False,
+             saveBmp=False, saveCutBmp=False, saveBWP=False, saveTxt=False):
+    """
+    将图片转成TXT
+    :param pictureRoute: 原始图片路径
+    :param folderRoute: 原始图片所在现在文件夹路径
+    :param saveRoute: 最终保存路径
+    :param flip: 反转显示
+    :param row: 裁剪后的行数
+    :param col: 裁剪后的列数
+    :param rowOffset: 裁剪时的行偏移
+    :param colOffset: 裁剪时的列偏移
+    :param showEveryStep: 显示每一次转化
+    :param saveEveryStep: 保存每一步转化
+    :param showBmp: 显示BMP
+    :param showCutBmp: 显示裁剪后的BMP
+    :param showBWP: 显示BWP
+    :param showTxt: 显示TXT
+    :param saveBmp: 保存BMP
+    :param saveCutBmp: 保存裁剪后的BMP
+    :param saveBWP: 保存BWP
+    :param saveTxt: 保存TXT
+    :return: TXT路径
+    """
+
+    if pictureRoute != '':
+        # 图片格式转化
+        BMP = bmp.Picture2Bmp(pictureRoute=pictureRoute, saveRoute=saveRoute)
+        bmpRoute = BMP.picture2Bmp(show=showEveryStep | showBmp, save=saveEveryStep | saveBmp)
+        print('Picture2Bmp done')
+        # 裁剪BMP
+        cutBMP = bmp.CutBmp(bmpRoute=bmpRoute, saveRoute=saveRoute)
+        cutBMPRoute = cutBMP.section(row=row, col=col, rowOffset=rowOffset, colOffset=colOffset,
+                                     show=showEveryStep | showCutBmp, save=saveEveryStep | saveCutBmp)
+        print('CutBmp done')
+        # BMP转成BWP
+        BWP = bmp.Bmp2BWP(bmpRoute=cutBMPRoute, saveRoute=saveRoute)
+        BWPRoute = BWP.bmp2BWP(flip=flip, show=showEveryStep | showBWP, save=saveEveryStep | saveBWP)
+        print('Bmp2BWP done')
+        # BWP转成TXT
+        TXT = bmp.PrintText(bmpRoute=BWPRoute, saveRoute=saveRoute)
+        TXTRoute = TXT.printText(show=showEveryStep | showTxt, save=saveEveryStep | saveTxt)
+        print('PrintText done')
+        return TXTRoute
+
+
 def main():
-    picture = bmp.PictureToBmp(pictureRoute='D:/Python Program/Image Processing/pictures/可达鸭斜着.jpeg',
-                               saveRoute='D:/Python Program/Image Processing/pictures/processed',
-                               folderRoute='D:/Python Program/Image Processing/pictures')
-    picture.jpegToBmp_Folder()
+
+    # demo_0(pictureRoute='D:/Python Program/Image Processing/pictures/可达鸭抱头.jpeg',
+    #        saveRoute='D:/Python Program/Image Processing/pictures/processed',
+    #        show=True, save=False)
+    # demo_1(folderRoute='D:/Python Program/Image Processing/pictures/processed',
+    #        saveRoute='D:/Python Program/Image Processing/pictures/processed/BWP',
+    #        flip=True, show=True, save=True)
+    # demo_2(bmpRoute='D:/Python Program/Image Processing/pictures/processed/BWP/可达鸭抱头.bmp',
+    #        saveRoute='D:/Python Program/Image Processing/pictures/processed/BWP/cut',
+    #        row=64, col=128, rowOffset=-50, colOffset=-20,
+    #        show=True, save=True)
+    # demo_3(bmpRoute='D:/Python Program/Image Processing/pictures/processed/BWP/cut/可达鸭抱头.bmp',
+    #        saveRoute='D:/Python Program/Image Processing/pictures/processed/BWP/cut/txt',
+    #        show=True, save=True)
+    jpeg2Txt(pictureRoute='D:/Python Program/Image Processing/pictures/可达鸭抱头.jpeg',
+             saveRoute='D:/Python Program/Image Processing/pictures/processed',
+             flip=True, row=64, col=128, rowOffset=-50, colOffset=-20,
+             showEveryStep=False, saveEveryStep=True,
+             showBmp=False, showCutBmp=False, showBWP=False, showTxt=False,
+             saveBmp=False, saveCutBmp=False, saveBWP=False, saveTxt=False)
     print('Done')
     return
 
